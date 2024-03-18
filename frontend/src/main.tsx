e2164575d4a4d0450c1c6cffae9785dc0993cd99
max <maxence.landa@gmail.com>
2024-03-18 16:48:08 +0100

    routes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Upload from './components/Upload.tsx'
+import Todos from './components/Todos.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Upload/>,
+  },
+  {
+    path: '/testapi',
+    element: <Todos/>,
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
