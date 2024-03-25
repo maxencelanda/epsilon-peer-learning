1c6b717a78386edc35e7adc15b9ff43b93542acd
max <maxence.landa@gmail.com>
2024-03-25 23:47:17 +0100

    front sends to back properly

--- frontend/src/components/Upload.tsx
+++ frontend/src/components/Upload.tsx
@@ -1,12 +1,21 @@
+import axios from "axios";
 import { useState } from "react"
 
 export default function Upload() {
 
   const [file, setFile] = useState(null)
 
-  const handleSubmit = e => {
+  const handleSubmit = async e => {
     if (file != null){
-      console.log(file[0].name)
+      console.log(file[0])
+      const formData = new FormData();
+      formData.append(
+        "fileUpload",
+        file[0]
+      );
+      const headers = {'Content-Type': file[0].type}
+      await axios.post("http://localhost:8000/uploadfile",formData,headers);
+
     } else {
       console.log("No file uploaded")
     }
