16d784a55a34185189969f176605db2b3cefa3bc
max <maxence.landa@gmail.com>
2024-04-08 06:55:47 +0200

    home component

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,7 +1,13 @@
+import { Link } from "react-router-dom";
+
 export default function Home() {
   return (
     <div>
-      
+      <nav className="grid grid-cols-7 text-center mt-2">
+        <Link to="/upload" className="col-start-3">Upload</Link>
+        <Link to="/login">Login</Link>
+        <Link to="/register">Register</Link>
+      </nav>
     </div>
   )
 }
